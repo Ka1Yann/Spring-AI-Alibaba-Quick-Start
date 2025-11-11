d68dd2f48f405646e97c172a6e892c0ec46821a9
ka1yan <changkaiyan@come-future.com>
2025-11-11 17:20:40 +0800

    feat:ChatClient generate a entity.

--- 01_quick_start/src/main/java/com/ka1yan/springai/zhipu/controller/ZhipuClientController.java
+++ 01_quick_start/src/main/java/com/ka1yan/springai/zhipu/controller/ZhipuClientController.java
@@ -1,5 +1,6 @@
 package com.ka1yan.springai.zhipu.controller;
 
+import com.ka1yan.springai.zhipu.entiry.User;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.DefaultChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,8 +35,8 @@ public class ZhipuClientController {
      * @param query 用户问题
      * @return 智能体流式响应
      */
-    @GetMapping("/chatClient")
-    public Flux<String> chatClient(@RequestParam(name = "query", defaultValue = "你是谁？") String query) {
+    @GetMapping("/demo")
+    public Flux<String> demo(@RequestParam(name = "query", defaultValue = "你是谁？") String query) {
         return chatClient.prompt()
                 .system("你是一个智能问答助手，准确回答用户的问题。")
                 .user(query)
@@ -43,4 +44,23 @@ public class ZhipuClientController {
                 .stream()
                 .content();
     }
+
+    /**
+     * 测试ChatClent-entity(); ChatClent直接返回实体对象
+     * <p>
+     * 原理：用户指定返回的内容后，Spring AI 会向 user prompt 后追加内容，使得 LLM 返回的内容必须为 Json 格式，并会指定 Json Schema，再由toEntity() 方法转化 Json 为实体类，从而实现调用 LLM 直接返回实体类。
+     * </p>
+     *
+     * @return User 生成的用户实体
+     */
+    @GetMapping("/entity")
+    public User LLM2Entity() {
+        return chatClient.prompt()
+                .system("你是一个智能问答助手，准确回答用户的问题。")
+                .user("生成一个用户，并为他赋予各种合理的属性，要求使用中文。")
+                .options(new DefaultChatOptions())
+                .call()
+                .entity(User.class);
+    }
+
 }
---
+++ 01_quick_start/src/main/java/com/ka1yan/springai/zhipu/entiry/User.java
@@ -0,0 +1,20 @@
+package com.ka1yan.springai.zhipu.entiry;
+
+import lombok.Data;
+
+/**
+ * user
+ *
+ * @author ka1yan
+ * @date 2025/11/11 17:13
+ * @description
+ */
+@Data
+public class User {
+    private String name;
+    private String email;
+    private String phone;
+    private String age;
+    private String sex;
+    private String address;
+}
